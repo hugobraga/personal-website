454e09c9064944725517e5f546000d13508b4a3f
Hugo Braga <hugobraga@gmail.com>
2021-02-08 21:42:43 -0400

    Adicionei texto e cor de fundo

--- react-website/src/pages/Microblog.js
+++ react-website/src/pages/Microblog.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+// import './Microblog.css';
+
 const Microblog = () => {
-  return <h2>Microblog</h2>;
+    return (
+        <div>
+            <h2>Hugo Braga</h2>
+            <h2>IoT Enthusiast</h2>
+            <h2>PhD in Computer Science</h2>
+            <h2>------------------------</h2>
+            <h2>I'm open to IoT Entrepreneurship!</h2>
+            <h2>Let's innovate in state of Amazonas</h2>
+            <h2>------------------------</h2>
+        </div>
+    );
 };
 
 export default Microblog;

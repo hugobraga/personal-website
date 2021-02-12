9f176bed1c8d5f0f274fef754d3fbfb303d9553c
Hugo Braga <hugobraga@gmail.com>
2021-02-12 17:17:39 -0400

    Adicionei a linha separadora e a foto arredondada

--- react-website/src/pages/Microblog.js
+++ react-website/src/pages/Microblog.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import SMButtons from '../components/SMButtons';
-// import Hr from 'react-native-hr-component';
-// import { Divider } from 'react-native-elements';
-
-// import './Microblog.css';
-{/* <Hr lineColor='#b3b3b3'/> */}
+import React from "react";
+import SMButtons from "../components/SMButtons";
+import "./Microblog.css";
+import ReactRoundedImage from "react-rounded-image";
+import MyPhoto from "./images/rosto.jpeg";
 
 const Microblog = () => {
-    return (
-        <div>
-            <h2>Hugo Braga</h2>
-            <h2>IoT Enthusiast</h2>
-            <h2>PhD in Computer Science</h2>
-            <h2>------------------------</h2>
-            <h2>I'm open to IoT Entrepreneurship!</h2>
-            <h2>Let's innovate in state of Amazonas</h2>
-            <h2>------------------------</h2>
-            <SMButtons />
-        </div>
-    );
+  return (
+    <div>
+      <ReactRoundedImage image={MyPhoto} roundedSize="0" imageWidth="110" imageHeight="110" />
+      <h2>Hugo Braga</h2>
+      <h2>IoT Enthusiast</h2>
+      <h2>PhD in Computer Science</h2>
+      <span className="linha-separadora">___________________________</span>
+      <h2>I'm open to IoT Entrepreneurship!</h2>
+      <h2>Let's innovate in state of Amazonas</h2>
+      <span className="linha-separadora">___________________________</span>
+      <SMButtons />
+    </div>
+  );
 };
 
 export default Microblog;

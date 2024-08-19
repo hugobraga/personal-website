06ed326b7bce434e1fb72945f4f6b01b33c9925c
Hugo Braga <hugobraga@gmail.com>
2024-08-19 18:34:57 -0400

    Created icons for Hugging Face and Kaggle

--- react-website/src/components/HomeRoundedButtons.js
+++ react-website/src/components/HomeRoundedButtons.js
@@ -4,6 +4,8 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
+import HuggingFaceIcon from "./icons/HuggingFaceIcon";
+import KaggleIcon from "./icons/KaggleIcon";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import "../pages/Microblog.css";
@@ -42,7 +44,23 @@ const HomeRoundedButtons = () => {
                 style={{ backgroundColor: '#000000', color: 'white' }} // X Black
             >
                 <XIcon />
-            </IconButton>            
+            </IconButton>
+            <IconButton
+                component={Link}
+                href="https://huggingface.co/hugobraga"
+                target="_blank"
+                style={{ backgroundColor: '#ffcc4d', color: '#664500' }} // Hugging Face Yellow
+            >
+                <HuggingFaceIcon />
+            </IconButton>
+            <IconButton
+                component={Link}
+                href="https://www.kaggle.com/bragahugo"
+                target="_blank"
+                style={{ backgroundColor: '#20BEFF', color: 'white' }} // Kaggle Blue
+            >
+                <KaggleIcon />
+            </IconButton>
 {/*}            <IconButton
                 component={Link}
                 href="https://x.com/_hugobraga"
@@ -56,11 +74,11 @@ const HomeRoundedButtons = () => {
                 href="https://www.facebook.com/hugovvbraga"
                 target="_blank"
                 style={{ backgroundColor: '#1877F2', color: 'white' }} // Facebook Blue
+
             >
-                <FacebookIcon />        
+                <FacebookIcon />
             </IconButton>*/}
         </div>
     );
 }
-
 export default HomeRoundedButtons;

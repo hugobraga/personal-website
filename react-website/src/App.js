08d2dc20ddd36766d1ce21aeb8259bff9884f757
Hugo Braga <hugobraga@gmail.com>
2024-08-22 21:28:26 -0400

    Adding google analytics into code.

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate
 } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+import config from './config'; // Import your config file
 
 import Microblog from './pages/Microblog';
 import Instalinks from './pages/Instalinks';
 
 const App = () => {
+  useEffect(() => {
+    const { measurementId } = config.googleAnalytics;
+    if (measurementId) {
+      ReactGA.initialize(measurementId);
+      ReactGA.send('pageview');
+    } else {
+      console.warn('Google Analytics Measurement ID is not set');
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Microblog />} />
         <Route path="/instalinks" element={<Instalinks />} />
-        {/* <Route path="/instalinks3" element={<Instalinks3 />} /> */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
---
+++ react-website/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  googleAnalytics: {
+    measurementId: process.env.REACT_APP_GA_MEASUREMENT_ID || 'G-BL9MS8M2N9', // Default fallback value
+  },
+  // Add more configuration values here
+};
+
+export default config;
